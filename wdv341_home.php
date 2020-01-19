2d6599c89364c06b4cf2a49f5b3dc5d9932bb847
mmason9589 <59899394+mmason9589@users.noreply.github.com>
2020-01-18 18:54:11 -0600

    Home page
    Home page

---
+++ wdv341_home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>WDV 341 Intro to PHP</title>
+	
+
+</head>
+
+<body>
+	
+<?php
+	
+	echo "<h1>WDV 341 Intro to PHP</h1>";
+	
+	echo "<h2>Welcome to Matt's Homework Page!</h2>";
+	
+	
+?>	
+
+	
+</body>
+</html>
